a2f19c15a22c418f286fe9d0416625bade7a0626
Louis <louisbossu23@gmail.com>
2020-01-20 20:07:51 +0100

    Fin initialisation et map GUI

--- Python_FrontEnd/mainWindow.py
+++ Python_FrontEnd/mainWindow.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtWidgets as Qtw
-from PyQt5.QtGui import QColor, QPixmap
+from PyQt5.QtGui import QColor, QPixmap, QPainter, QPen, QBrush
 from PyQt5 import QtCore as Qt
 from parseSerial import Parser
 import Goose_GUI as GUI
@@ -21,24 +21,87 @@ class Comm(Qtw.QFrame):
             #print(name + " : " + str(value))
             self.mess.setText(name + " : " + str(value))
 
-class initialisation_setting(Qtw.QWidget):
+class initialisation_settings(Qtw.QFrame):
     newPositionCalibration = Qt.pyqtSignal(dict)  # self.newPositionCalibration( { PosEnum.POS_X : X, PosEnum.POS_Y : Y, PosEnum.POS_THETA : Th} )
 
-    def __init__(self):
-        
+    def __init__(self, mainWindow):
         super().__init__()
+        self.parent_window = mainWindow
+        self.initUI()
 
     def initUI(self):
         self.setFrameShadow(Qtw.QFrame.Plain)
         self.setFrameShape(Qtw.QFrame.StyledPanel)
-        self.myLayout = Qtw.QHBoxLayout(self)
-        self.label = Qtw.QLabel("Settings")
-        self.myLayout.addWidget(self.label)
-        self.slider_x = Qtw.QSlider(Qt.Horizontal)
-        self.slider_x.set
-        self.slider_y = Qtw.QSlider(Qt.Horizontal)
+        self.myLayout = Qtw.QGridLayout(self)
+        self.label = Qtw.QLabel("Settings (X,Y,Theta) :")
+        self.myLayout.addWidget(self.label, 2, 1)
+        self.slider_x = Qtw.QSlider(Qt.Qt.Horizontal)
+        self.myLayout.addWidget(self.slider_x, 2, 2)
+        self.slider_x.setMinimum(0)
+        self.slider_x.setMaximum(3833)
+        self.slider_x.setValue(3833//2)
+        self.slider_y = Qtw.QSlider(Qt.Qt.Horizontal)
+        self.myLayout.addWidget(self.slider_y, 2, 3)
+        self.slider_y.setMinimum(0)
+        self.slider_y.setMaximum(3277)
+        self.slider_y.setValue(3277//2)
+        self.dial_theta = Qtw.QDial()
+        self.myLayout.addWidget(self.dial_theta, 2, 4)
+        self.dial_theta.setMinimum(-180)
+        self.dial_theta.setMaximum(180)
+        self.dial_theta.setValue(0)
+        self.label_init = Qtw.QLabel("Initialisation")
+        self.myLayout.addWidget(self.label_init, 1, 1)
+        self.check_init = MySwitch()
+        self.myLayout.addWidget(self.check_init, 1, 3)
 
-'''
+        self.slider_x.valueChanged.connect(self.valuechanged)
+        self.slider_y.valueChanged.connect(self.valuechanged)
+        self.dial_theta.valueChanged.connect(self.valuechanged)
+        self.check_init.clicked.connect(self.statebutton)
+
+    def statebutton(self):
+        if self.check_init.isChecked():
+            self.check_init.clicked.connect(lambda : self.parent_window.sendMessage.emit("y"))
+        else:
+            self.check_init.clicked.connect(lambda : self.parent_window.sendMessage.emit("n"))
+
+    def valuechanged(self):
+            return self.slider_x.Value(), self.slider_y.Value(), self.dial_theta.Value()
+
+class MySwitch(Qtw.QPushButton):
+    def __init__(self, parent = None):
+        super().__init__(parent)
+        print('init')
+        self.setCheckable(True)
+        self.setMinimumWidth(66)
+        self.setMinimumHeight(22)
+
+    def paintEvent(self, event):
+        label = "ON" if self.isChecked() else "OFF"
+        bg_color = Qt.Qt.green if self.isChecked() else Qt.Qt.red
+
+        radius = 10
+        width = 32
+        center = self.rect().center()
+
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+        painter.translate(center)
+        painter.setBrush(QColor(0,0,0))
+
+        pen = QPen(Qt.Qt.black)
+        pen.setWidth(2)
+        painter.setPen(pen)
+
+        painter.drawRoundedRect(Qt.QRect(-width, -radius, 2*width, 2*radius), radius, radius)
+        painter.setBrush(QBrush(bg_color))
+        sw_rect = Qt.QRect(-radius, -radius, width + radius, 2*radius)
+        if not self.isChecked():
+            sw_rect.moveLeft(-width)
+        painter.drawRoundedRect(sw_rect, radius, radius)
+        painter.drawText(sw_rect, Qt.Qt.AlignCenter, label)
+
 class map_GUI(Qtw.QWidget):
     def __init__(self):
         super().__init__()
@@ -62,7 +125,6 @@ class map_GUI(Qtw.QWidget):
         self.resize(pixmap.width(), pixmap.height())
         self.show()
 
-'''
 class DebugMessage(Qtw.QFrame):
     def __init__(self):
         super().__init__()
@@ -86,25 +148,26 @@ class MainWindow(Qtw.QWidget):
         self.mainLayout.addWidget(self.commWidget)
         self.debugWidget=DebugMessage()
         self.mainLayout.addWidget(self.debugWidget)
-        #self.mapWidget=map_GUI()
-        #self.mainLayout.addWidget(self.mapWidget)
-
+        self.settings=initialisation_settings(self)
+        self.mainLayout.addWidget(self.settings)
         self.sendButton = Qtw.QPushButton("Click click")
         self.mainLayout.addWidget(self.sendButton)
         self.sendButton.clicked.connect(lambda : self.sendMessage.emit("!"))
+        self.mapWidget = map_GUI()
+        self.mainLayout.addWidget(self.mapWidget)
 
-        self.posCompution = PositionComputation(self)
-
-        self.parserThread=Parser(self)
-        self.sendMessage.connect(self.parserThread.sendMessage) # When MainWindow emit message, the parser catch them and send on serial port
-        self.parserThread.newData.connect(self.commWidget.update) # When parser emit newData, 
-        self.parserThread.newDebug.connect(self.debugWidget.update)
-        self.parserThread.newDebug.connect(print) # When parser emit message, print in console
-        #self.parserThread.newMovement.connect(self.mapWidget.updatePosition)
-
-        self.parserThread.newData.connect(self.posCompution.dataReception)
-
-        #self.posCompution.newPosition.connect(self.mapWidget.updatePosition)
-
-        
-        self.parserThread.start()
+        # self.posCompution = PositionComputation(self)
+        #
+        # self.parserThread=Parser(self)
+        # self.sendMessage.connect(self.parserThread.sendMessage) # When MainWindow emit message, the parser catch them and send on serial port
+        # self.parserThread.newData.connect(self.commWidget.update) # When parser emit newData,
+        # self.parserThread.newDebug.connect(self.debugWidget.update)
+        # self.parserThread.newDebug.connect(print) # When parser emit message, print in console
+        # #self.parserThread.newMovement.connect(self.mapWidget.updatePosition)
+        #
+        # self.parserThread.newData.connect(self.posCompution.dataReception)
+        #
+        # #self.posCompution.newPosition.connect(self.mapWidget.updatePosition)
+        #
+        #
+        # # self.parserThread.start()
